03aa47738d29ad884cdf93cac7d1620b6aa63917
Guil2007 <217774650+Guil2007@users.noreply.github.com>
2025-08-03 18:20:30 +0000

    Desafio Super trunfo aventureiro

--- super_trunfo_2.c
+++ super_trunfo_2.c
@@ -5,6 +5,7 @@
 
 int main() {
     //Declara as variáveis para cada atributo da cidade.
+
     char codigo_estado1; char codigo_estado2;
     char codigocarta1[4]; char nomecidade1[20]; char codigocarta2[4]; char nomecidade2[20];
     unsigned long int populacao1; int pontos_turisticos1; unsigned long int populacao2; int pontos_turisticos2;
@@ -130,28 +131,112 @@ int main() {
     printf(" \n");
     printf(" \n");
 
-    printf("Digite [1] para Combater População\n");
+    // Escolha o atributo do Combate
+
+    printf("Escolha qual atributo você quer Combater\n");
+    printf(" Digite 1 para Combater em População \n");
+    printf(" Digite 2 para Combater em Pontos Turísticos \n");
+    printf(" Digite 3 para Combater em Área \n");
+    printf(" Digite 4 para Combater em PIB \n");
+    printf(" Digite 5 para Combater em PIB per Capta \n");
+    printf(" Digite 6 para Combater em Densidade Demográfica \n");
+    printf(" Digite 7 para Combater em Super Trunfo! \n");
+
+    scanf("%d", &batalha);
+
+    switch (batalha)
+    {
+    case 1:
+        if (populacao1 > populacao2)
+         { printf("A Carta número 1  Maceio venceu o desafio de População \n");
+         } else {
+            printf("A carta número 2 Fortaleza venceu o desafio de População \n");
+    }
+
+        printf(" A Populacão da Carta 1 é %lu Habitantes \n", populacao1);
+        printf(" A Populacão da Carta 2 é %lu Habitantes \n", populacao2);
     
-    scanf( "%d", &batalha);
+        break;
 
-   // printf("A Carta vitoriosa em População é: %d \n", resultadopop);
-   // printf("A Carta vitoriosa em área é: %d \n", resultadoarea);
-    // printf("A Carta vitoriosa em PIB interno Bruto é: %d \n", resultadopib);
-    // printf("A Carta vitoriosa em Pontos turísticos é: %d \n", resultadotur);
-    // printf("A Carta vitoriosa em Densidade Demográfica é: %d \n", resultadodens);
-   //  printf("A Carta vitoriosa em PIB per Capta é: %d \n", resultadopibpc);
-   // printf("A Carta vitoriosa em Super Trunfo é: %d \n", resultadosuper);
+    
+    case 2:
+       if (pontos_turisticos1 > pontos_turisticos2)
+         { printf("A Carta número 1  Maceio venceu o desafio de Pontos Turísticos \n");
+        } else {
+            printf("A carta número 2 Fortaleza venceu o desafio de Pontos Turísticos \n");
+    }
 
-    printf(" A Populacão da Carta 1 é %lu Habitantes \n", populacao1);
-    printf(" A Populacão da Carta 2 é %lu Habitantes \n", populacao2);
+        printf(" Os Pontos Turísticos da Carta 1 são %d \n", pontos_turisticos1);
+        printf(" Os Pontos Turísticos da Carta 2 são %d \n", pontos_turisticos2);
+    
+        break;
 
-    if (populacao1 > populacao2)
-    { printf("A Carta número 1  Maceio venceu o desafio de População");
-        /* code */
-    } else {
-        printf("A carta número 2 Fortaleza venceu o desafio de População");
+    case 3:
+         if (area1 > area2)
+         { printf("A Carta número 1  Maceio venceu o desafio de Área \n");
+        } else {
+            printf("A carta número 2 Fortaleza venceu o desafio de Área \n");
+    }
+
+        printf(" A Área da Carta 1 é %.2f Km2 \n", area1);
+        printf(" A Área da Carta 2 é %.2f Km2 \n", area2);   
+    
+        break;
+
+     case 4:
+         if (pib1 > pib2)
+         { printf("A Carta número 1  Maceio venceu o desafio de PIB \n");
+        } else {
+            printf("A carta número 2 Fortaleza venceu o desafio PIB \n");
+    }
+
+        printf(" O PIB da Carta 1 é %.2f Reais \n", pib1);
+        printf(" O PIB da Carta 2 é %.2f Reais \n", pib2);  
+
+        break;   
+
+    case 5:
+       if (pibpc1 > pibpc2)
+         { printf("A Carta número 1  Maceio venceu o desafio de PIB per Capta \n");
+        } else {
+            printf("A carta número 2 Fortaleza venceu o desafio de PIB per Capta \n");
+    }
+   
+        printf(" O PIB per Capta da Carta 1 é %.2f Reais \n", pibpc1);
+        printf(" O PIB per Capta da Carta 2 é %.2f Reais \n", pibpc2); 
+   
+        break;
+
+    case 6:
+         if (dens1 < dens2)
+         { printf("A Carta número 1  Maceio venceu o desafio de Densidade Demográfica \n");
+        } else {
+            printf("A carta número 2 Fortaleza venceu o desafio de Densidade Demográfica \n");
+    }
+   
+        printf(" A Densidade Demográfica da Carta 1 é %.2f Habitantes por Km2 \n", dens1);
+        printf(" A Densidade Demográfica da Carta 2 é %.2f Habitantes por Km2 \n", dens2);    
+    
+        break;
+
+    case 7:
+         if (super1 > super2)
+         { printf("A Carta número 1  Maceio venceu o desafio de Super trunfo! \n");
+        } else {
+            printf("A carta número 2 Fortaleza venceu o desafio de Super Trunfo! \n");
     }
     
+        printf(" O Super Trunfo da Carta 1 é %.2f \n", super1);
+        printf(" O Super Trunfo da Carta 2 é %.2f \n", super2);        
+    
+        break;
+
+
+    default:
+        printf("Opção Inválida \n");
+    }
+
+     
     printf(" \n");
     printf(" \n");
 
